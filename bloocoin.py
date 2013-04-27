b9573f6ab9ce1931ea4b35b525948c51777b131c
plausibility <chris@gibsonsec.org>
2013-04-28 01:56:50 +1000

    General purpose refactor, client simplified with removal of duplicate code

--- bloocoin.py
+++ bloocoin.py
@@ -12,12 +12,12 @@ class BlooClient:
         self.type = "client"
         self.ver = "1.05"
         self.cmds = {
-            "transactions":self.transactions,
-            "addr":self.addr,
-            "coins":self.coins,
-            "send":self.sendcoin,
-            "help":self.help,
-            }
+            "transactions": self.transactions,
+            "addr": self.addr,
+            "coins": self.coins,
+            "send": self.sendcoin,
+            "help": self.help,
+        }
         self.update()
         return
     
@@ -32,24 +32,42 @@ class BlooClient:
                 continue
         return
 
-    def transactions(self):
+    def _send_json(self, data, buff=1024):
+        """ This function does most of the leg work for the script,
+            since almost all functionality required by the client
+            sends and receives some JSON data to/from the server.
+        """
         s = socket.socket()
         s.settimeout(3)
-        while True:
-            try:
-                s.connect((self.ip, self.port))
-                break
-            except:
-                print "Server seems down... trying again in 10 seconds."
-                time.seep(10)
-        s.send(json.dumps({"cmd":"transactions", "addr":self.addr_get(), "pwd":self.pwd_get()}))
-        while True:
-            data = s.recv(1024)
-            if data:
-                print data
-            else:
-                break
-        return
+        try:
+            s.connect((self.ip, self.port))
+        except IOError as e:
+            print "[ ! ] Problem connecting to server: {0}".format(e)
+            return
+        if isinstance(data, dict):
+            data = json.dumps(data)
+        try:
+            s.send(data)
+        except IOError as e:
+            print "[ ! ] Problem sending data to server: {0}".format(e)
+            return
+        reply = None
+        try:
+            reply = s.recv(buff)
+        except IOError as e:
+            print "[ ! ] Problem in recv(): {0}".format(e)
+        return reply
+
+    def transactions(self):
+        data = self._send_json({
+            "cmd": "transactions",
+            "addr": self.addr_get(),
+            "pwd":self.pwd_get()
+        })
+        if data:
+            print data
+        else:
+            print "Something went wrong getting transactions! :("
 
     def addr(self):
         with open("bloostamp", 'rb') as file:
@@ -65,77 +83,44 @@ class BlooClient:
             return file.read().split(":")[1]
         
     def coins(self):
-        s = socket.socket()
-        s.settimeout(3)
-        while True:
-            try:
-                s.connect((self.ip, self.port))
-                break
-            except:
-                print "Server seems to be down... trying again in 10 seconds."
-                time.sleep(10)
-                continue
-        s.send(json.dumps({ "cmd":"my_coins", "addr":self.addr_get(), "pwd":self.pwd_get()}))
-        print "You currently have", s.recv(1024), "coins."
-        s.close()
-        return
+        data = self._send_json({
+            "cmd": "my_coins",
+            "addr": self.addr_get(),
+            "pwd": self.pwd_get()
+        })
+        if data:
+            print "You currently have", data, "coins."
+        else:
+            print "Something went wrong getting coin listings! :("
      
     def sendcoin(self):
-        s = socket.socket()
-        s.settimeout(3)
-
-        # Connect
-        while True:
-            try:
-                s.connect((self.ip, self.port))
-                break
-            except:
-                print "Server seems to be down... trying again in 10 seconds."
-                time.sleep(10)
-
-        # Send request.               
         cmd = self.cmd.split()
         amount = cmd[1]
         to = cmd[2]
         addr = self.addr_get()
         pwd = self.pwd_get()
-        s.send(json.dumps({ "cmd":"send_coin", "amount":amount, "to":to, "addr":addr, "pwd":pwd }))
-
-        # Get response
-        try:
-            data = s.recv(1024)
-        except socket.timeout:
-            print "Server is not responding."
-            s.close()
-            return
+        data = self._send_json({
+            "cmd": "send_coin",
+            "amount": amount,
+            "to": to,
+            "addr": addr,
+            "pwd": pwd
+        })
         if data:
             print data
-        s.close()
-        return
+        else:
+            print "Unable to send your coin! Uh oh! :("
     
     def register(self, addr, pwd):
-        s = socket.socket()
-        while True:
-            try:
-                s.connect((self.ip, self.port))
-                break
-            except:
-                print "Server seems to be down... trying again in 10 seconds."
-                time.sleep(10)
-        s.send(json.dumps({ "cmd":"register", "addr":addr, "pwd":pwd }))
-        s.settimeout(3)
-        try:
-            check = s.recv(1024)
-        except socket.timeout:
-            print "Server is not responding."
-            s.close()
-            return
-        if not check == "True":
-            print check
-            s.close()
-            return
-        s.close()
-        return
+        data = self._send_json({
+            "cmd": "register",
+            "addr": addr,
+            "pwd": pwd
+        })
+        if data.strip() == "True":
+            print "Registration successful! Your address is: {0}".format(addr)
+        else:
+            print "Registration failed! '{0}' :(".format(data)
 
     def help(self):
         print """
@@ -147,35 +132,19 @@ transactions - Shows all transactions you have made.
 To generate a new BlooCoin address simply delete your bloostamp file and relaunch bloocoin."""
 
     def update(self):
-        s = socket.socket()
-        s.settimeout(3)
-        try:
-            s.connect((self.ip, self.port))
-        except:
-            s.close()
-            print "Couldn't connect to server.\n"
-            return
-        s.send(json.dumps({ "ver": self.ver, "cmd": "update", "type": self.type }))
-        s.settimeout(3)
-        try:
-            response = s.recv(1024)
-        except socket.timeout:
-            print "Server seems to be offline.\n"
-            s.close()
-            return
-        s.close()
-        if response[0] == "0":
-            print "Your %s is running the latest version.\n" % self.type
-            return
-        
-        if response[0] == "1":
+        data = self._send_json({
+            "ver": self.ver,
+            "cmd": "update",
+            "type": self.type
+        })
+        if data[0] == "0":
+            print "Your {0} is running the latest version.\n".format(self.type)
+        elif data[0] == "1":
             print "A new version is available. It can be downloaded at:\nhttps://raw.github.com/Max00355/BlooCoin/master/bloocoin.py\n"
-            return
-
-        if response[0] == "2":
-            print response[1:] + "\n"
-            return
-        return
+        elif data[0] == "2":
+            print data[1:] + "\n"
+        else:
+            print "Unknown data returned from update: {0}".format(data)
 
 if __name__ == "__main__":
     if not os.path.exists("bloostamp"):
